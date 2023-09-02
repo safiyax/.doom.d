45790a4154d01bcc61b410455e679176ae39d5cb
Zeeshan Hooda <zee@sparselabs.org>
2023-09-01 22:49:32 -0600

    initial commit

---
+++ custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ein:markdown-max-image-size '(nil))
+ '(ein:output-area-inlined-images t)
+ '(fancy-splash-image
+   "/Users/zee/.emacs.d/modules/ui/doom-dashboard/banners/default.png")
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(mailcap-user-mime-data nil)
+ '(org-display-remote-inline-images 'download)
+ '(org-image-actual-width nil)
+ '(package-selected-packages '(quarto-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'upcase-region 'disabled nil)
